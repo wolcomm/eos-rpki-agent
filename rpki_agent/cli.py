8a1d2bc09ce0496fcbdfe463b2078e3192b33d88
Ben Maddison <benm@workonline.co.za>
2019-03-18 15:28:20 +0200

    create agent outline

--- rpki_agent/cli.py
+++ rpki_agent/cli.py
@@ -18,17 +18,46 @@ from __future__ import unicode_literals
 import argparse
 import sys
 
+import eossdk
+
+
+class RpkiAgent(eossdk.AgentHandler):
+    """An EOS SDK based agent that creates routing policy objects."""
+
+    def __init__(self, sdk, args):
+        """Initialise the agent instance."""
+        self.name = self.__class__.__name__
+        self.tracer = sdk.Tracer(self.name)
+        self.tracer.trace0("Setting up {}".format(self.name))
+        self.args = args
+        self.agent_mgr = sdk.get_agent_mgr()
+        eossdk.AgentHandler.__init__(self, self.agent_mgr)
+        self.tracer.trace0("Set up done")
+
+    def on_initialized(self):
+        """Handle the initialised event from the AgentHandler."""
+        self.tracer.trace0("Connecting to {}".format(self.args.cache_url))
+
 
 def main():
     """Run the RpkiAgent."""
     try:
+        # get an instance of the EOS SDK
+        sdk = eossdk.Sdk()
+    except Exception as e:
+        return 3
+    try:
+        # parse any cli arguments
         args = _get_args()
+        # create an instance of the RpkiAgent
+        agent = RpkiAgent(sdk, args)  # noqa: W0612
+        # enter the sdk event-loop
+        sdk.main_loop()
     except KeyboardInterrupt:
         return 1
     except Exception as e:
         print(e)
         return 2
-    print("Connecting to {}".format(args.cache_url))
     return
 
 
